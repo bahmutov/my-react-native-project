d556dfc74ad299aafececeb2d5dda02494fa3fa4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-06-16 14:42:12 -0400

    add delay and fixture test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -12,7 +12,12 @@ it('loads list of users', () => {
 })
 
 it('shows loading indicator', () => {
+  // slow down the response by 1 second
+  // https://on.cypress.io/intercept
   cy.intercept('/users', (req) => {
+    // use bundled Bluebird library
+    // which has utility method .delay
+    // https://on.cypress.io/promise
     return Cypress.Promise.delay(1000).then(() => req.continue())
   }).as('users')
   cy.visit('/')
@@ -24,5 +29,15 @@ it('shows loading indicator', () => {
 it('shows mock data', () => {
   cy.intercept('/users', { fixture: 'users.json' }).as('users')
   cy.visit('/')
+  cy.get('[data-testid=user]').should('have.length', 3)
+})
+
+it('shows loading indicator (mock)', () => {
+  cy.intercept('/users', {
+    fixture: 'users.json', delay: 1000
+  }).as('users')
+  cy.visit('/')
+  cy.get('[data-testid=loading]').should('be.visible')
+  cy.get('[data-testid=loading]').should('not.exist')
   cy.get('[data-testid=user]').should('have.length', 3)
 })
