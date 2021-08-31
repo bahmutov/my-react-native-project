d32902703d2858f5cb958a8f0ec8a0220d00eefb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-08-30 23:07:33 -0400

    bump Cypress and add a few test tweaks

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,13 +1,20 @@
 /// <reference types="cypress" />
 
-it('loads list of users', () => {
+it('loads a list of users', () => {
+  cy.visit('/')
+  cy.get('[data-testid=user]')
+    .should('have.length.gt', 3)
+})
+
+it('spy on the network load', () => {
   cy.intercept('/users').as('users')
   cy.visit('/')
   cy.wait('@users').its('response.body')
     .should('be.an', 'Array')
     .and('have.length.gt', 5)
     .then(users => {
-      cy.get('[data-testid=user]').should('have.length', users.length)
+      cy.get('[data-testid=user]')
+        .should('have.length', users.length)
     })
 })
 
@@ -22,6 +29,20 @@ it('shows loading indicator', () => {
   }).as('users')
   cy.visit('/')
   cy.get('[data-testid=loading]').should('be.visible')
+  cy.get('[data-testid=loading]').should('not.exist')
+  cy.wait('@users')
+})
+
+it('shows the loading indicator', () => {
+  // slow down the response by 1 second
+  // https://on.cypress.io/intercept
+  cy.intercept('/users', (req) => {
+    return req.continue(res => res.setDelay(1000))
+  }).as('users')
+  cy.visit('/')
+  // the loading indicator should be visible at first
+  cy.get('[data-testid=loading]').should('be.visible')
+  // the disappear
   cy.get('[data-testid=loading]').should('not.exist')
   cy.wait('@users')
 })
@@ -42,8 +63,11 @@ it('shows loading indicator (mock)', () => {
   cy.get('[data-testid=user]').should('have.length', 3)
 })
 
-it('handles network error', () => {
-  cy.intercept('/users', { forceNetworkError: true })
+it('handles network error after 1 second', () => {
+  cy.intercept('/users', (req) => {
+    return Cypress.Promise.delay(1000)
+      .then(() => req.reply({ forceNetworkError: true }))
+  })
   // observe the application's behavior
   // in our case, the app simply logs the error
   cy.visit('/', {
@@ -51,7 +75,8 @@ it('handles network error', () => {
       cy.spy(win.console, 'error').as('logError')
     },
   })
-  cy.get('@logError').should('have.been.called')
+  cy.get('[data-testid=loading]').should('be.visible')
   // confirm the loading indicator goes away
   cy.get('[data-testid=loading]').should('not.exist')
+  cy.get('@logError').should('have.been.called')
 })
